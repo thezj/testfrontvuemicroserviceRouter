08689d6db9f55debea85d7051f4a24c4292a4f63
thezj <thezj@qq.com>
2018-04-12 11:26:05 +0800

    添加了代理配置，可在前端当前域名下发送api

--- vueappframeroute/dev.js
+++ vueappframeroute/dev.js
@@ -4,6 +4,16 @@
  module.exports = merge(common, {
      devtool: 'inline-source-map',
      devServer: {
-         port: 8000
+         port: 8000,
+         proxy: {
+             "/app1/": {
+                 target: "http://localhost:8001",
+                 pathRewrite:{'^/app1' : ''}
+             },
+             "/app2/": {
+                target: "http://localhost:8002",
+                pathRewrite:{'^/app2' : ''}
+            }
+         }
      }
  });
--- vueappframeroute/src/home.vue
+++ vueappframeroute/src/home.vue
@@ -2,6 +2,14 @@
   <div>welcome</div>
 </template>
 <script>
-  export default {
-  };
+export default {
+  mounted() {
+    axios.post("/app1/getinfo").then(({ data }) => {
+      console.log(data);
+    });
+    axios.post("/app2/getinfo").then(({ data }) => {
+      console.log(data);
+    });
+  }
+};
 </script>
